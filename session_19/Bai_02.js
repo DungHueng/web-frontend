515209b2d89643297d01d4fb703af671d959ccf4
DungHueng <dunghoang26lc@gmail.com>
2025-03-03 15:48:40 +0700

    Update Bai_02.js

--- session_19/Bai_02.js
+++ session_19/Bai_02.js
@@ -1,6 +1,6 @@
-let text= prompt("Mời bạn nhập một từ bất kì");
-if(text.includes(0||1||2||3||4||5||6||7||8||9)){
-    alert(text+ " không phải là chữ cái");
-}else{  
-    alert("ký tự "+ text +" là chữ cái");
+let text = String(prompt("nhập ký tự bất kì: "));
+if(text.includes("0")|| text.includes("1")|| text.includes("2")|| text.includes("3")|| text.includes("4")|| text.includes("5")|| text.includes("6")|| text.includes("7")|| text.includes("8")|| text.includes("9")){
+    alert(text + " không phải chữ cái");
+} else {
+    alert("ký tự " + text + " là chữ cái");
 }
